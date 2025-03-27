cb58dddf774e21a694f231bb1e342d53410c13c0
Dada <142039148+Dada09876@users.noreply.github.com>
2025-03-27 10:27:37 -0400

    Update Cannibal.c

--- Scripts/4_World/Modded Items/Food/Cannibal.c
+++ Scripts/4_World/Modded Items/Food/Cannibal.c
@@ -21,25 +21,41 @@ class DadaCannibal_Base extends HumanSteakMeat
 		super.SetActions();
 		
 		RemoveAction(ActionForceFeed);
-		RemoveAction(ActionEatMeat);
-		AddAction(ActionDrinkCookingPot);
+		AddAction(ActionEatMeat);
+		RemoveAction(ActionDrinkCookingPot);
 
 		AddAction(ActionCreateIndoorFireplace);
 		AddAction(ActionCreateIndoorOven);
 	}
 };
 
+class DadaCannibal_RawFoodCan_Colorbase extends DadaCannibal_Base
+{
+};
+
+class DadaCannibal_PreservedFoodCan_Colorbase extends DadaCannibal_Base 
+{
+  override bool CanDecay()
+	{
+		return false;
+	}
+};
+
+class Dada_PreservedFoodCan_Colorbase_Opened: Edible_Base
+{
+	override bool CanProcessDecay()
+	{
+		return !( GetAgents() & eAgents.FOOD_POISON );
+	}
+};
+
 class DadaCannibal_EyeballSoup extends DadaCannibal_Base
-{};
+{
+	AddAction(ActionDrinkCookingPot);
+};
 
 class DadaCannibal_EntrailsSpaghetti extends DadaCannibal_Base
 {
-	override void SetActions()
-	{
-		super.SetActions();
-		RemoveAction(ActionDrinkCookingPot);
-		AddAction(ActionEatMeat);
-	}	
 };
 
 class DadaCannibal_Jerky extends DadaCannibal_Base
@@ -63,21 +79,10 @@ class DadaCannibal_Jerky extends DadaCannibal_Base
 	{
 		return true;
 	}
-
-	override void SetActions()
-	{
-		super.SetActions();
-		RemoveAction(ActionDrinkCookingPot);
-		AddAction(ActionEatMeat);	
-	}
 };
 
 class DadaCannibal_WasteBread extends DadaCannibal_Base
 {
-	override void SetActions()
-	{
-		super.SetActions();
-		RemoveAction(ActionDrinkCookingPot);
-		AddAction(ActionEatMeat);
-	}	
 };
+
+
