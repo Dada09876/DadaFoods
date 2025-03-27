1900eb8e37e221399b96f88580dfb3bdade78700
Dada <142039148+Dada09876@users.noreply.github.com>
2025-03-27 15:31:33 -0400

    Update Cannibal.c

--- Scripts/4_World/Recipes/Food/Cannibal/Cannibal.c
+++ Scripts/4_World/Recipes/Food/Cannibal/Cannibal.c
@@ -248,50 +248,42 @@ class CraftDadaCannibal_WasteBread extends Craft_DadaTemp1Stage1_BaseRecipe
 	}
 };
 
-class CraftDadaWasteBread_Pita extends DadaCannibal_Base {};
+class CraftDadaWasteBread_Pita extends 
 
-class CraftDadaWasteBread_Loaf extends DadaCannibal_Base {};
+class CraftDadaWasteBread_Loaf extends 
 
-class CraftDadaWasteBread_Baguette extends DadaCannibal_Base {};
+class CraftDadaWasteBread_Baguette extends 
 
-class CraftDadaWasteBread_Pasta extends DadaCannibal_Base {};
+class CraftDadaWasteBread_Pasta extends 
 
-class CraftDadaWasteBread_Pizza extends DadaCannibal_Base {};
+class CraftDadaWasteBread_Pizza extends 
 
-class CraftDadaWasteBread_PizzaBase extends DadaCannibal_Base {};
+class CraftDadaWasteBread_PizzaBase extends 
 
-class CraftDadaCannibalDish_Rigatoni extends DadaCannibal_Base {};
+class CraftDadaCannibalDish_Rigatoni extends 
 
-class CraftDadaCannibalDish_Chili extends DadaCannibal_Base {};
+class CraftDadaCannibalDish_Chili extends 
 
-class CraftDadaCannibalDish_Pie extends DadaCannibal_Base {};
+class CraftDadaCannibalDish_Pie extends 
 
-class CraftDadaCannibalSoup_TomatoSoup extends DadaCannibal_Base {};
+class CraftDadaCannibalSoup_TomatoSoup extends 
 
-class CraftDadaCannibalSoup_Ragout extends DadaCannibal_Base {};
+class CraftDadaCannibalSoup_Ragout extends 
 
-class CraftDadaCannibalDish_GroundMeat extends DadaCannibal_Base {};
+class CraftDadaCannibalDish_GroundMeat extends 
 
-class CraftDadaCannibalSoup_TomatoSauce extends DadaCannibal_Base {};
+class CraftDadaCannibalSoup_TomatoSauce extends 
 
-class CraftDadaCannibalDish_Spaghetti extends DadaCannibal_Base {};
+class CraftDadaCannibalDish_Spaghetti extends 
 
-class CraftDadaCannibalDish_Cannelloni extends DadaCannibal_Base {};
+class CraftDadaCannibalDish_Cannelloni extends 
 
-class CraftDadaCannibalDish_Bowties extends DadaCannibal_Base {};
+class CraftDadaCannibalDish_Bowties extends 
 
-class CraftDadaCannibalDish_Lasagna extends DadaCannibal_Base {};
+class CraftDadaCannibalDish_Lasagna extends 
 
-class CraftDadaCannibalDish_PulledPork extends DadaCannibal_Base {};
+class CraftDadaCannibalDish_PulledPork extends 
 
-class CraftDadaCannibalDish_Sausage extends DadaCannibal_Base {};
+class CraftDadaCannibalDish_Sausage extends 
 
-class CraftDadaCannibalDrink_MilkShake extends DadaCannibal_Base 
-{
-	override void SetActions()
-	{
-	super.SetActions();
-	AddAction(ActionDrink);
-	RemoveAction(ActionEatMeat);
-	}
-};
+class CraftDadaCannibalDrink_MilkShake extends 
